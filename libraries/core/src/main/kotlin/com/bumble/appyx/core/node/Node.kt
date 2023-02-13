81cfab5ae6ada374e8df3a09e7df282839b74864
charlie-niekirk <chaz759@protonmail.com>
2023-02-13 18:45:15 +0000

    Fixed source breaking change

--- libraries/core/src/main/kotlin/com/bumble/appyx/core/node/Node.kt
+++ libraries/core/src/main/kotlin/com/bumble/appyx/core/node/Node.kt
@@ -167,8 +167,8 @@ open class Node(
         }
     }
 
-    override fun getLifecycle(): Lifecycle =
-        nodeLifecycle.lifecycle
+    override val lifecycle: Lifecycle
+        get() = nodeLifecycle.lifecycle
 
     override fun updateLifecycleState(state: Lifecycle.State) {
         if (lifecycle.currentState == state) return
